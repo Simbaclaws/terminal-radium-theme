d716ec12dbc643c84bbbfab4173f1b301a6d4502
hylke <hylke.hellinga@gmail.com>
2024-08-30 20:04:10 +0200

    Initial Commit

---
+++ Gnome/gnome.sh
@@ -0,0 +1,4 @@
+# Set Radium theme colors
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:{profile_id}/ background-color '#101317'
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:{profile_id}/ foreground-color '#d4d4d5'
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:{profile_id}/ palette "['#0a0d11', '#f87070', '#37d99e', '#ffe59e', '#7ab0df', '#c397d8', '#50cad2', '#525559', '#191d22', '#ff8e8e', '#79dcaa', '#ffeda6', '#87bdec', '#b68acb', '#63b3ad', '#d4d4d5']"
---
+++ Hyper/hyper.js
@@ -0,0 +1,28 @@
+module.exports = {
+    config: {
+        // Radium theme colors
+        backgroundColor: '#101317',
+        foregroundColor: '#d4d4d5',
+        cursorColor: '#d4d4d5',
+        colors: {
+            black: '#0a0d11',
+            red: '#f87070',
+            green: '#37d99e',
+            yellow: '#ffe59e',
+            blue: '#7ab0df',
+            magenta: '#c397d8',
+            cyan: '#50cad2',
+            white: '#525559',
+            lightBlack: '#191d22',
+            lightRed: '#ff8e8e',
+            lightGreen: '#79dcaa',
+            lightYellow: '#ffeda6',
+            lightBlue: '#87bdec',
+            lightMagenta: '#b68acb',
+            lightCyan: '#63b3ad',
+            lightWhite: '#d4d4d5'
+        },
+        // other config options...
+    },
+    // plugins, etc...
+};
